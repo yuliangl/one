5432cfacd6715824656d9aaf5bac34c76f749504
onhway <yigeren010@gmail.com>
2024-07-15 05:18:24 -0700

    Edit Dual link list.

--- sylar/list.cc
+++ sylar/list.cc
@@ -23,37 +23,113 @@ Node::Node(int val, Node* p, Node* n)
     
 }
 
-Node::Node(const Node& node)
-    :value(node.value)
-    ,prev(new Node)
-    ,next(new Node){
-
-    printf("copy construstor ...................\n");
-    *prev = *(node.prev);
-    *next = *(node.next);
-
-}
 
 Node::~Node(){
-    delete prev;
-    delete next;
-}
-
-Node& Node::operator=(const Node& node){
-    printf("copy ==========...................\n");
-    value = node.value;
-    Node* tmpNode(new Node(node));
-
     if(prev)
         delete prev;
     if(next)
         delete next;
-
-    prev = new Node(*(tmpNode->prev));
-    next = new Node(*(tmpNode->next));
-
-    return *this; 
 }
+
+
+DuLinkList::DuLinkList()
+    :m_head(new Node())
+{
+}
+
+DuLinkList::DuLinkList(const DuLinkList& list){
+    Node* node = list.getHead();
+    m_head = new Node(*(list.getHead()));
+    Node* self = m_head;        
+
+    while(node->next){
+        node = node->next;
+        self->next = new Node(*node);
+        Node* tmp = self;
+        self = self->next;
+        self->prev = tmp;
+
+        if(node->next == list.getHead()){
+            m_head->prev = self;
+            break;
+        }
+    }
+}
+
+DuLinkList::~DuLinkList(){
+}
+
+DuLinkList& DuLinkList::operator=(const DuLinkList& list){
+    return *this;
+}
+
+void DuLinkList::push_back(Node* node){
+    if(m_head->prev == nullptr){
+        m_head->next = node;
+        m_head->prev = node;
+        node->prev = m_head;
+        node->next = m_head;
+        return;
+    }
+
+    Node* end = m_head->prev;
+    end->next = node;
+    node->prev = end;
+    node->next = m_head;
+    m_head->prev = node;
+
+}
+
+void DuLinkList::push_front(Node* node){
+    if(m_head->next == nullptr){
+        m_head->next = node;
+        m_head->prev = node;
+        node->prev = m_head;
+        node->next = m_head;
+        return;
+    }
+
+    Node* front = m_head->next;
+    front->prev = node;
+    node->next = front;
+    node->prev = m_head;
+    m_head->next = node;
+}
+
+Node* DuLinkList::pop_back(){
+    Node* back = m_head->prev;
+    Node* end = back->prev;
+
+    end->next = m_head;
+    m_head->prev = end;
+
+    return back;
+}
+Node* DuLinkList::pop_front(){
+    Node* front = m_head->next;
+    Node* first = front->next;
+
+    m_head->next = first;
+    first->prev = m_head;
+
+    return front;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- sylar/list.h
+++ sylar/list.h
@@ -8,12 +8,12 @@ namespace sylar{
 class Node {
 public:
     Node();
-    Node(int val);
+    explicit Node(int val);
     Node(int val, Node* p, Node* n);
-    Node(const Node& node);
+    Node(const Node& node) = default;
     ~Node();
 
-    Node& operator=(const Node& node);
+    Node& operator=(const Node& node) = default; 
 
     int value;
     Node* prev;
@@ -24,14 +24,16 @@ public:
 class DuLinkList {
 public:
     DuLinkList();
+    DuLinkList(const DuLinkList& list);
     ~DuLinkList();
 
+    DuLinkList& operator=(const DuLinkList& list);
     void push_back(Node* node);
     void push_front(Node* node);
-    void pop_back();
-    void pop_front();
-    
+    Node* pop_back();
+    Node* pop_front();
 
+    Node* getHead() const {return m_head;}
 private:
     Node* m_head;
 };
